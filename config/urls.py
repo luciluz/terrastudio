35f54d57f90d3a17db9655974c767a1122b82fbe
luciluz <luciruzveloso@gmail.com>
2026-01-30 19:24:00 -0300

    add robots for google

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,18 @@ from propiedades.views import inicio, detalle_propiedad, nosotros, catalogo, ser
 from django.contrib.sitemaps.views import sitemap
 from propiedades.sitemaps import PropiedadSitemap, StaticViewSitemap
 
+from django.http import HttpResponse
+
+def robots_txt(request):
+    lines = [
+        "User-agent: *",
+        "Disallow: /admin/",
+        "Allow: /",
+        "",
+        "Sitemap: https://terrastudio.cl/sitemap.xml",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
+
 sitemaps = {
     'propiedades': PropiedadSitemap,
     'estaticas': StaticViewSitemap,
@@ -26,6 +38,7 @@ urlpatterns = [
     path('servicios/', servicios, name='servicios'),
 
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', robots_txt),
 ]
 
 if settings.DEBUG:
